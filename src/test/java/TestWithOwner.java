7e5bafd3a7d0998a4f6ab9a5812c811f790c47af
catcat4er <kerimov-t90@mail.ru>
2022-02-21 19:28:02 +0400

    added test with owner

--- src/test/java/TestWithOwner.java
+++ src/test/java/TestWithOwner.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("simple")
-public class TestWithPageObjects extends TestBase {
+public class TestWithOwner extends TestBase {
 
     @Test
     public void firstJenkinsTest() {
