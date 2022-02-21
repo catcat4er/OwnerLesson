312b124c7a44008f89791a432df8ea89763145da
catcat4er <kerimov-t90@mail.ru>
2022-02-21 19:28:41 +0400

    added Credential class

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,17 +14,22 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
 //        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
 //        Configuration.browser = System.getProperty("browser","chrome");
 //        Configuration.browserVersion = System.getProperty("browserVersion");
-        String user = System.getProperty("user");
-        String password = System.getProperty("password");
+
+//        String user = System.getProperty("user");
+//        String password = System.getProperty("password");
         String url = System.getProperty("url");
+
+        String user = config.user();
+        String password = config.password();
 
         String mainUrl = "https://" + user + ":" + password + "@" + url;
         Configuration.remote = mainUrl;
